8be2f0641faed8b9308b66df4bd7b2088558eebc
mnzhukova <zhukova.m94@mail.ru>
2025-11-19 01:08:04 +0300

    add new files: constants, player_actions, utils. Main: create game_state

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -2,12 +2,20 @@
 """
 Модуль main — точка входа в игру.
 """
+from .constants import ROOMS
+
+ROOMS
 
 def main():
     """Основная функция игры."""
     print("Первая попытка запустить проект!")
 
-
+game_state = {
+        'player_inventory': [], # Инвентарь игрока
+        'current_room': 'entrance', # Текущая комната
+        'game_over': False, # Значения окончания игры
+        'steps_taken': 0 # Количество шагов
+}
 
 if __name__ == "__main__":
     main()
